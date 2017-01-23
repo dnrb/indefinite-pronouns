f439dbcde1337fa42fa21f8f8f4ac63324de1c5d
dnrb <barendbeekhuizen@gmail.com>
2017-01-23 11:46:25 -0500

    cleaned up script oc.r

--- oc.r
+++ oc.r
@@ -1,109 +1,84 @@
-# based on script on Croft's website - refer
-# TODO:
-# - clean up and write into functions
-# - find out how to plot unicode in legends
+# based on the R scripts retrieved from
+# http://www.unm.edu/~wcroft/MDS.html (January 23rd, 2017)
 
-library(pscl)
 library(oc)
-library(gdata)
 library(ggplot2)
 
 language.labs = c('ar','bg','bs','cz','da','de','el','en','es','et','fi',
   'fr', 'he', 'hr', 'hu', 'id', 'it', 'ja', 'nl', 'no', 'pl', 'pr', 'ro',
   'ru', 'sl','sr','sv','tr','vi','zh')
-
-name <- ''
+name <- 'oc_SPLIT'
 folder <- '.'
-#
-name <- args[2]
-folder <- args[1]
 fp <- sprintf('%s/%s.csv',folder, name)
-# read folder and name from command line or define them manually
 
-
-#
-gold.fp = sprintf('%s/%s_gold.csv', folder, name)
-gold = read.csv(gold.fp)
-#
-selected.functions = c('DN','IN')
-selected.sits = gold$gold %in% selected.functions
-selected.sits = rep(TRUE,nrow(gold))
-
-data.txt <- read.csv(fp,header=TRUE)
-data.txt = data.txt[selected.sits,]
-nCUTs <- length(2:ncol(data.txt))
-tt <- data.txt [,-1]
-names <- colnames(tt)
-T <- (tt[,1:nCUTs])
-hr <- rollcall(T, yea=1, nay=6, missing=9,notInLegis=8,desc= title,) 
-   
-minVOTES <- 5
-min_freq = 5
-LOP <- (1/nrow(data.txt))* min_freq
+##############
+# parameters #
+##############
+onto = 'thing' # 'body'
+# ontological category to consider
+ndims = 2
+# number of dimensions in the OC analysis
+min.terms = 5
 # minimum number of positive terms a situation must have
+min.freq = 5
+# minimum number of situations a term must be applicable to
+parameters = sprintf('onto=%s_dim=%d', onto, ndims)
+
+#############
+# read data #
+#############
+original.data = read.csv(sprintf('%s_gold.csv', name))
+selected.sits = original.data$ontological == onto
+
+full.data = read.csv(fp,header=TRUE)
+data = full.data[selected.sits,]
+nCUTs = length(2:ncol(data))
+tt = data[,-1]
+names = colnames(tt)
+T = (tt[,1:nCUTs])
+hr = rollcall(T, yea=1, nay=6, missing=9,notInLegis=8,desc=title,) 
+min.sits = (1/nrow(data)) * min.freq
 # minimal percentage of situations a term may occur in positive
 
-##
-# ACTUAL OC
-##
-ndims = 3
-result <- oc(hr,dims=ndims, minvotes=minVOTES, lop=LOP, polarity = 1:ndims, verbose = TRUE)
-df = data.frame(result$legislators[,7],
-  row.names = 1:nrow(result$legislators))
+#############
+# actual OC #
+#############
+result = oc(hr, dims=ndims, minvotes= min.terms,
+  lop= min.sits, polarity = 1:ndims, verbose = TRUE)
+df = data.frame(result$legislators[,7], row.names = 1:nrow(result$legislators))
 colnames(df) = c('dim.1')
-for (i in 1:ndims-1) {
-  colname = sprintf('dim.%d',i+1)
-  df[[colname]] = result$legislators[,7+i]
-}
-write.csv(df, sprintf('%s/%s_%ddim.csv', folder, name, ndims))
+for (i in 1:ndims-1) { df[[sprintf('dim.%d',i+1)]] = result$legislators[,7+i] }
+write.csv(df, sprintf('%s/%s_%s.csv', folder, name, parameters))
 
-coordinates = read.csv(sprintf('%s/%s_%ddim.csv', folder, name, ndims), sep = ',', header = TRUE)
+###############
+# plot resuls #
+###############
+coordinates.filename = sprintf('%s/%s_%s.csv', folder, name, parameters)
+coordinates = read.csv(coordinates.filename, sep = ',', header = TRUE)
 labels.fp = sprintf('%s/%s_labels.csv', folder, name)
 labels = read.csv(labels.fp)
-gold.fp = sprintf('%s/%s_gold.csv', folder, name)
-gold = read.csv(gold.fp)
-index.fp = sprintf('%s/%s_situations.csv', folder, name)
-situations = read.csv(index.fp)
-data = cbind(coordinates, labels[selected.sits,], gold[selected.sits,])
-data$situations = paste(situations[selected.sits,]$utterance,
-  situations[selected.sits,]$word)
+data.a = labels[selected.sits,]
+data.a$dim.1 = coordinates$dim.1
+data.a$dim.2 = coordinates$dim.2
+data.a$annotation = original.data[selected.sits,]$annotation
+data.a$situations = paste(original.data[selected.sits,]$utt,
+  original.data[selected.sits,]$word)
 
-###
-# PLOTTING
-###
+xlims = c(min(-data.a$dim.1),max(-data.a$dim.1))
+ylims = c(min(-data.a$dim.2),max(-data.a$dim.2))
 
-xlims = c(min(data$dim.1),max(data$dim.1))
-ylims = c(min(data$dim.2),max(data$dim.2))
-
-for (language in 5:34) {
-  data.sub = data[data[,language] != '',]
-  #
+for (language in 2:31) {
+  data.sub = data.a[data.a[,language] != '',]
   top = sort(table(data.sub[,language]), decreasing = TRUE)
-  top.n = names(top[top > nrow(data.txt)/100 * min_freq])
+  top.n = names(top[top > nrow(data.a)/100 * 2])#min_freq])
   data.subsub = droplevels(data.sub[data.sub[,language] %in% top.n,])
-  q = qplot(dim.1, dim.2, color = data.subsub[,language], data = data.subsub)
+  q = qplot(-dim.1, -dim.2, color = data.subsub[,language], label = data.subsub[,language], data = data.subsub, geom = 'text')
   q = q + xlim(xlims)
   q = q + ylim(ylims)
-  ggsave(sprintf('%s/%s_L%d.pdf', folder, name, language), q, height = 7, width = 7)  
+  q = q + theme(axis.title.x = element_blank(), axis.title.y = element_blank())
+  q = q + guides(color = (show=FALSE), size = (show= FALSE))
+  ggsave(sprintf('%s/%s_%s_L%d.pdf', folder, parameters, name, language), q, height = 5.5, width = 4)
 }
   
-centroid = aggregate(cbind(dim.1,dim.2) ~ gold, data = data, FUN = mean)
-q = qplot(dim.1, dim.2, label = gold, data = centroid, geom = 'text')
-ggsave(sprintf('%s/%s_centroid.pdf', folder, name), q)
-
-q = qplot(dim.1, dim.2, label = situations, data = data, geom = 'text')
-ggsave(sprintf('%s/%s_situations.pdf', folder, name), q, height = 15, width = 15)
-
-###
-#
-###
-
-
-# analyses of gradient
-sorted.data = data[order(data$dim.1),]
-sorted.data$indices = 1:nrow(sorted.data)
-#
-for (i in 4:34) {
-  print(language.labs[i-3])
-  print(as.numeric(sorted.data[,i]))
-}
+q = qplot(-dim.1, -dim.2, color = annotation, label = annotation, size = 10, data = data.a, geom = 'text') + xlim(xlims) + ylim(ylims) + theme(axis.title.x = element_blank(), axis.title.y = element_blank()) + guides(color = (show=FALSE), size = (show= FALSE))
+ggsave(sprintf('%s/%s_%s_annotations.pdf', folder, parameters, name), q, height = 5, width = 3.6)

98cbe3873b3d07802099380fddf1d54582c06a92
Julia <jreneewatson@gmail.com>
2017-01-23 16:28:19 -0500

    Added documentation to analyze_clustering.py; Modified README.md to be more in line with the paper.

--- README.md
+++ README.md
@@ -8,14 +8,11 @@ Breakdown of scripts and how to use them by section of paper:
 		- usage: python create_dev_test_split.py
 		- description: Splits exemplars in data/full_set.csv into a dev_set and test_set of equal sizes and writes them to data/dev_set.tsv and data/test_set.csv.
 
-* Are all functions equally important?
-	- PLACEHOLDER
-
 * Are the functions at the right level of granularity?
 	- analyze_clustering.py
 		- usage: python analyze_clustering.py data/test_set.tsv data/stemming_dictionary.csv
 		- dependencies: data.py
-		- description: Prints a clustering summary for each parameter setting (see description of parameters in the class docstring for the data class in data.py). Each summary consists of the Adjusted Rand Score followed by a frequency table between Haspelmath function and clusters found by the clustering algorithm.
+		- description: Prints a clustering summary for each parameter setting (see description of parameters in the class docstring for the data class in data.py). Each summary consists of the Adjusted Rand Score with the Haspelmath gold labels followed by a confusion matrix between Haspelmath functions and clusters found by the clustering algorithm.
 
 * The perspective of a similarity space
 	- data.py
@@ -25,7 +22,10 @@ Breakdown of scripts and how to use them by section of paper:
 		- usage: Rscript oc.r
 		- description: Generates OCMDS plots based on info in oc__labels.csv, oc__gold.csv and oc_.csv.
 
-* Returning to the semantic map
+Other scripts used in our research
+
+* Semantic maps
+
 	- graph_inferring.py
 		- usage: python graph_inferring.py
 		- dependencies: data.py, graph_parameters.py (see graph_parameters.py file to modify behavior)
--- analyze_clustering.py
+++ analyze_clustering.py
@@ -15,13 +15,34 @@ from sklearn.metrics import confusion_matrix
 from collections import defaultdict as dd
 
 def get_k(clustering, depth = 10):
-    last = clustering[-depth: , 2]
-    acceleration = np.diff(last, 2)
-    acceleration_rev = acceleration[::-1]
-    k = acceleration_rev.argmax() + 2
-    return k
+	"""
+	(ndarray, int) -> int
+	clustering: ndarray -- linkage matrix representing hierarchical clustering
+	depth: int -- the maximum depth to traverse clustering
+	
+	Returns the number of clusters to extract from the hierarchical clustering
+	using the elbow method.
+	"""
+	last = clustering[-depth: , 2]
+	acceleration = np.diff(last, 2)
+	acceleration_rev = acceleration[::-1]
+	k = acceleration_rev.argmax() + 2
+	return k
 
 def get_cluster_assignments(sim_matrix, parameters):
+	"""
+	(np.array, list of int) -> list of int
+	sim_matrix: list of list of float -- similarity matrix between exemplars
+	parameters: list of parameters in the format ["method:method_name", 
+			"algo:algo_name", "k:num_clusters", "damping:damping"]
+			where order doesn't matter
+			(k and damping only relevant for certain clustering methods)
+			the possible values for each parameter are listed in the
+			function below.
+	
+	Returns a list of integers. The integer at each index of the list corresponds
+	to the cluster number of the exemplar at the same index in sim_matrix.
+	"""
 
 	algorithm = next((re.split(':',f)[1] for f in parameters if f[:4] == 'algo'), 'ap')
 	# from { 'hierarchical', 'kmeans', 'ap', 'ward' }
@@ -47,11 +68,26 @@ def get_cluster_assignments(sim_matrix, parameters):
 	return cluster_assignments
 
 def evaluate_clustering(cluster_assignments, gold):
+	"""
+	(list of int, list of str) -> float
+	cluster_assignments: list of int -- cluster numbers for each exemplar
+	gold: list of str -- Haspelmath codes for each exemplar
+
+	Returns the adjusted rand score between the groupings cluster_assignments
+	and gold. Filters out all exemplars marked as 'UF'.
+	"""
 	non_uf = np.where(gold != 'UF')
 	cluster_assignments_sub, gold_sub = cluster_assignments[non_uf], gold[non_uf]
 	return adjusted_rand_score(gold_sub, cluster_assignments_sub)
 
 def print_confusion_matrix(cluster_assignments, gold):
+	"""
+	(list of int, list of str) -> None
+	cluster_assignments: list of int -- cluster numbers for each exemplar
+	gold: list of str -- Haspelmath codes for each exemplar
+
+	Prints out the confusion matrix between cluster_assignments and gold.
+	"""
 	m = dd(lambda : [0 for i in range(len(set(cluster_assignments)))])
 	for c,g in zip(cluster_assignments,gold):
 		m[g][c] += 1
@@ -60,10 +96,26 @@ def print_confusion_matrix(cluster_assignments, gold):
 
 
 def write_cluster_assignments(cluster_assignments, parameters):
+	"""
+	(list of int, list of str) -> None
+	cluster_assignments: list of int -- cluster numbers for each exemplar
+	parameters: list of str -- list of parameters
+	
+	Writes the cluster assignments to a file named based on the contents
+	of parameters.
+	"""
 	with open('clustering_%s.csv' % '_'.join(sorted(parameters)),'w') as fh:
 		fh.write('assignment\n%s' % '\n'.join([str(s) for s in cluster_assignments])) 
 
 def sim(d1,d2, parameters):
+	"""
+	(list of str, list of str, list of str) -> float
+	d1: list of str -- list of terms for each language used in a situation
+	d2: list of str -- list of terms for each language used in a situation
+	parameters: list of str -- list of parameters
+
+	Returns the similarity score between d1 and d2.
+	"""
 	comparisons = [(e1==e2) if not 'SPLIT' in parameters else
 		((len(set(e1) & set(e2))/len(set(e1) | set(e2))) if len(e1+e2) > 0 else 0)
 		for e1,e2 in zip(d1,d2) 
@@ -71,6 +123,17 @@ def sim(d1,d2, parameters):
 	return (sum(comparisons)/len(comparisons)) if len(comparisons) > 0 else 0
 
 def get_similarity_matrix(d, parameters, oix):
+	"""
+	(data, list of str, list of int) -> list of list of float
+	d: data -- data to construct similarity matrix from
+	parameters: list of str -- list of parameters
+	oix: list of int -- list of indices in data to consider (used to only
+			include situations corresponding to particular ontological
+			categories)
+	
+	Returns a similarity matrix between the exemplars in d that are at
+	indices in oix in d.data.
+	"""
 	#
 	data_sub = d.data[oix]
 	similarity_matrix = np.ones((data_sub.shape[0],data_sub.shape[0]))
@@ -81,9 +144,15 @@ def get_similarity_matrix(d, parameters, oix):
 	return similarity_matrix
 
 def single_exp():
-	data_path = sys.argv[1]
-	stem_dict_path = sys.argv[2]
-	parameters = sys.argv[3:]
+	"""
+	Runs a single clustering experiment based on a list of arguments.
+	"""
+
+	data_path = sys.argv[1]    # path to data set
+	stem_dict_path = sys.argv[2]	# path to stemming dictionary
+	parameters = sys.argv[3:]	# list of parameters in format ["method:method_name", 
+					#"algo:algo_name", "k:num_clusters", "damping:damping"]
+					# see get_cluster_assignments for more details
 	d = data(data_path, stem_dict_path, parameters)
 	#
 	onto_cat = next((re.split(':',f)[1] for f in parameters if f[:4] == 'onto'),None)
@@ -97,8 +166,11 @@ def single_exp():
 	print_confusion_matrix(cluster_assignments, d.annotation[oix])
 
 def comparative_exp():
-	data_path = sys.argv[1]
-	stem_dict_path = sys.argv[2]
+	"""
+	Runs a series of clustering experiments for different parameter settings.
+	"""
+	data_path = sys.argv[1]	   # path to data set
+	stem_dict_path = sys.argv[2]    # path to stemming dictionary
 	parameters = ['SPLIT', 'noUF']
 	d = data(data_path, stem_dict_path, parameters)
 	#

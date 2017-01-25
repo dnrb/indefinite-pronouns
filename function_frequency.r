f018a1fcc2423185de03bb5a58cf7141483a91b5
drnb <barendbeekhuizen@gmail.com>
2017-01-25 15:39:09 -0500

    added smoothing for H calculation in function_frequency.r

--- function_frequency.r
+++ function_frequency.r
@@ -1,6 +1,6 @@
 library(entropy)
 #
-data = read.csv('data/dev_set.tsv', sep = '\t')
+data = read.csv('data/test_set.tsv', sep = '\t')
 data.sub = droplevels(data[data$source != 'excluded' & data$type %in% c('body', 'thing') & data$annotation != 'UF',])
 
 ###################
@@ -19,5 +19,6 @@ fisher.test(xtabs(~ data.sub$type + data.sub$annotation),workspace = 2e7)
 df = data.frame(prop.table(xtabs(~ data.sub$type + data.sub$annotation),1))
 p.function.body = df[df$data.sub.type == 'body',]$Freq
 p.function.thing = df[df$data.sub.type == 'thing',]$Freq
-H.body = -sum(p.function.body * log2(p.function.body))
-H.thing = -sum(p.function.thing * log2(p.function.thing))
+smooth = 0.000001
+H.body = -sum((p.function.body+smooth) * log2(p.function.body+smooth))
+H.thing = -sum((p.function.thing+smooth) * log2(p.function.thing+smooth))
